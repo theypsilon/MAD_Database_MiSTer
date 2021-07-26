bbf84e8d420293436787177331827ef18f682f60
José manuel Barroso Galindo <theypsilon@gmail.com>
2021-07-26 21:48:59 +0200

    Fix.

--- .github/generate_db.py
+++ .github/generate_db.py
@@ -216,7 +216,7 @@ class MadReader:
         return self._data
 
     def repeated(self):
-        return [array for array in self._repeated if len(array) > 1]
+        return {key: value for (key, value) in self._repeated if len(value) > 1}
 
 def create_orphan_branch(branch):
     run_succesfully('git checkout -qf --orphan %s' % branch)
